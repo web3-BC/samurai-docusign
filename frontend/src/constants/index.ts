8481f8a9643526fbc34a1bbdf758de7a3b1220ad
shun0305 <shunpg.dev@gmail.com>
2023-09-23 09:05:09 -0400

    chore: add contract abi

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -19,3 +19,192 @@ export const RPC_LIST = {
 
 export const PDFJS_WORKER_SOURCE =
   "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.6.172/pdf.worker.min.js";
+
+export const ABI = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "recipient",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "encryptedCid",
+        type: "string",
+      },
+    ],
+    name: "ContractIssued",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "recipient",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "encryptedCid",
+        type: "string",
+      },
+    ],
+    name: "ContractSigned",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    name: "encryptedCidToContract",
+    outputs: [
+      {
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "recipient",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "encryptedCid",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "hashedEmail",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "encryptedSymmetricKey",
+        type: "string",
+      },
+      {
+        internalType: "bool",
+        name: "isSigned",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "encryptedCid",
+        type: "string",
+      },
+    ],
+    name: "getContractByEncryptedCid",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "sender",
+            type: "address",
+          },
+          {
+            internalType: "address",
+            name: "recipient",
+            type: "address",
+          },
+          {
+            internalType: "string",
+            name: "encryptedCid",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "hashedEmail",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "encryptedSymmetricKey",
+            type: "string",
+          },
+          {
+            internalType: "bool",
+            name: "isSigned",
+            type: "bool",
+          },
+        ],
+        internalType: "struct ContractManager.Contract",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "encryptedCid",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "hashedEmail",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "encryptedSymmetricKey",
+        type: "string",
+      },
+    ],
+    name: "issueContract",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "encryptedCid",
+        type: "string",
+      },
+    ],
+    name: "signContract",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
