9aa450fc5cfdf11fa47f9de50dacd6bf25439a30
Tetsu Kinomura <tetsukinomura@gmail.com>
2023-09-23 06:12:32 -0400

    feat: create stepper

---
+++ frontend/src/app/issuers/contracts/new/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export enum Steps {
+  FileUpload = 1,
+  RegisterSinger,
+  VerifyHuman,
+  GetLink,
+}
+
+const CreateContractPage = () => {
+  return (
+    <main className="h-[calc(100vh-70px)] bg-slate-50 py-16">
+    </main>
+  );
+};
+
+export default CreateContractPage;
