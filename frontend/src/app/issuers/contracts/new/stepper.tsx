af2d7df2fb8f245014402f11192c05700e522cc1
Asuma Yamada <yamadaasuma@gmail.com>
2023-09-23 18:52:53 -0400

    fix: color

--- frontend/src/app/issuers/contracts/new/stepper.tsx
+++ frontend/src/app/issuers/contracts/new/stepper.tsx
@@ -32,7 +32,7 @@ const Stepper = ({ currentStep, className }: StepperProps) => {
       </li>
       <li
         className={`${
-          currentStep >= Steps.FileUpload ? "text-blue-600 " : ""
+          currentStep >= Steps.FileUpload ? "text-secondary-500 " : ""
         }after:border-1 flex items-center after:mx-6 after:hidden after:h-1 after:w-full after:border-b after:border-gray-200 after:content-[''] dark:after:border-gray-700 sm:after:inline-block md:w-full xl:after:mx-10`}
       >
         <span className="flex items-center after:mx-2 after:text-gray-200 after:content-['/'] dark:after:text-gray-500 sm:after:hidden">
@@ -54,7 +54,7 @@ const Stepper = ({ currentStep, className }: StepperProps) => {
       </li>
       <li
         className={`${
-          currentStep >= Steps.RegisterSinger ? "text-blue-600 " : ""
+          currentStep >= Steps.RegisterSinger ? "text-secondary-500 " : ""
         }after:border-1 flex items-center after:mx-6 after:hidden after:h-1 after:w-full after:border-b after:border-gray-200 after:content-[''] dark:after:border-gray-700 sm:after:inline-block md:w-full xl:after:mx-10`}
       >
         <span className="flex items-center after:mx-2 after:text-gray-200 after:content-['/'] dark:after:text-gray-500 sm:after:hidden">
@@ -76,7 +76,7 @@ const Stepper = ({ currentStep, className }: StepperProps) => {
       </li>
       <li
         className={`${
-          currentStep >= Steps.GetLink ? "text-blue-600 " : ""
+          currentStep >= Steps.GetLink ? "text-secondary-500 " : ""
         }flex items-center`}
       >
         {currentStep > Steps.GetLink ? (
--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="w-full bg-white py-10">
-      <h2 className="text-secondary-500 text-center text-xl font-bold">
+    <footer className="w-full bg-white py-10 border-gray-100 border-t">
+      <h2 className="text-center text-xl font-bold text-secondary-500">
         Samurai Sign
       </h2>
     </footer>
