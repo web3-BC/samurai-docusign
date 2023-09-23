2b130272d66387f077be988948f0ac3225c3e560
Tetsu Kinomura <tetsukinomura@gmail.com>
2023-09-23 08:15:55 -0400

    feat: input email

--- frontend/src/app/issuers/contracts/new/page.tsx
+++ frontend/src/app/issuers/contracts/new/page.tsx
@@ -18,6 +18,7 @@ export enum Steps {
 const CreateContractPage = () => {
   const [currentStep, setCurrentStep] = useState<Steps>(Steps.FileUpload);
   const [file, setFile] = useState<File>();
+  const [email, setEmail] = useState<string>("");
 
   const { upload } = useIPFS();
   const { encrypt } = useLit();
@@ -56,15 +57,30 @@ const CreateContractPage = () => {
           switch (currentStep) {
             case Steps.FileUpload:
               return (
-                <FileUploader
-                  file={file}
-                  setFile={setFile}
-                >
-		  <StepChanger />
-		</FileUploader>
+                <FileUploader file={file} setFile={setFile}>
+                  <StepChanger />
+                </FileUploader>
               );
             case Steps.RegisterSinger:
-              return <div>RegisterSigner</div>;
+              return (
+                <div className="w-1/3">
+                  <label
+                    htmlFor="email"
+                    className="mb-2 block font-medium text-gray-900 dark:text-white"
+                  >
+                    Signer email
+                  </label>
+                  <input
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                    required
+                  />
+                  <StepChanger />
+                </div>
+              );
             case Steps.VerifyHuman:
               return <div>VerifyHuman</div>;
             case Steps.GetLink:
