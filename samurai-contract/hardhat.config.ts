94c33272dbfc0c37ef4bfbcc6c6c06be65ebf57f
Asuma Yamada <yamadaasuma@gmail.com>
2023-09-23 09:27:50 -0400

    feat add scrollSepoila config

--- samurai-contract/hardhat.config.ts
+++ samurai-contract/hardhat.config.ts
@@ -24,6 +24,10 @@ const config: HardhatUserConfig = {
       accounts: [`0x${process.env.PRIVATE_KEY}`],
       allowUnlimitedContractSize: true,
     },
+    scrollSepoila: {
+      url: "https://1rpc.io/scroll/sepolia",
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
   },
 };
 
