ded2363c8cda4af17665e2a622b623d2909350c8
hidetomo-shimaoka <hyde2000s@gmail.com>
2023-09-23 04:14:09 -0400

    setup frontend config

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,17 @@
+const path = require("path");
+
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: path.join(__dirname, "tsconfig.json"),
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "import/no-anonymous-default-export": ["off"],
+    "@typescript-eslint/no-var-requires": ["off"],
+  },
+};
+
+module.exports = config;
