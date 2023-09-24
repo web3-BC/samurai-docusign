e5641dc399fdba9ce5966fdb7598b998ac935b32
Asuma Yamada <yamadaasuma@gmail.com>
2023-09-24 06:06:27 -0400

    fix: property

--- frontend/src/app/signers/success/page.tsx
+++ frontend/src/app/signers/success/page.tsx
@@ -11,7 +11,7 @@ const SucccessPage = () => {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="1"
+            strokeWidth="1"
           >
             <path
               strokeLinecap="round"
@@ -30,7 +30,7 @@ const SucccessPage = () => {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              stroke-width="2"
+              strokeWidth="2"
             >
               <path
                 strokeLinecap="round"
